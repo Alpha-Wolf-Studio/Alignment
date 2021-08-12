adbef47c9e68fbd9cce1596fdd3a916d52574668
Federico Olive <FedericoOlive@gmail.com>
2021-08-11 23:57:40 -0300

    Add animation on Main Menu.

---
+++ Assets/Scripts/Menu/ComponentRotate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+public class ComponentRotate : MonoBehaviour
+{
+    enum Axis { X, Y, Z}
+    private Rigidbody rb;
+    [SerializeField] private Axis axisForce = Axis.X;
+    [SerializeField] private float speed;
+    private Vector3 force;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        switch (axisForce)
+        {
+            case Axis.X:
+                force = transform.right* speed;
+                break;
+            case Axis.Y:
+                force = transform.up* speed;
+                break;
+            case Axis.Z:
+                force = transform.forward* speed;
+                break;
+            default:
+                Debug.Log("Axis excede el límite.", gameObject);
+                break;
+        }
+
+        rb.AddTorque(force);
+    }
+}
---
+++ Assets/Scripts/Menu/NaveCapsula.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NaveCapsula : MonoBehaviour
+{
+    private Camera cam;
+    private float onTime;
+    [SerializeField] private float transitionTime;
+    [SerializeField] private float rotationTime;
+    private Vector3 camInitialPos;
+    private Vector3 navePos;
+    private Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        cam = Camera.main;
+    }
+    void Start()
+    {
+        Time.timeScale = 0.5f;
+        camInitialPos = cam.transform.position;
+        navePos = transform.position;
+    }
+    public void StartAnim()
+    {
+        anim.SetTrigger("Launch");
+    }
+    public void SetCameraChild()
+    {
+        cam.transform.SetParent(gameObject.transform);
+        anim.enabled = false;
+    }
+    public void StartCamera()
+    {
+        StartCoroutine(MoveCam());
+    }
+    IEnumerator MoveCam()
+    {
+        while (onTime < transitionTime)
+        {
+            Debug.Log("Moviendo");
+            navePos = transform.position;
+            onTime += Time.unscaledDeltaTime;
+            cam.transform.position = Vector3.Slerp(camInitialPos, navePos, onTime / transitionTime);
+            yield return null;
+        }
+        onTime = 0;
+        StartCoroutine(RotateCam());
+    }
+    IEnumerator RotateCam()
+    {
+        Quaternion rotation = cam.transform.localRotation;
+        while (onTime < rotationTime)
+        {
+            Debug.Log("Rotando");
+            onTime += Time.unscaledDeltaTime;
+            cam.transform.localRotation = Quaternion.Lerp(rotation, Quaternion.identity, onTime / rotationTime);
+            yield return null;
+        }
+        anim.enabled = true;
+        onTime = 0;
+    }
+    public void EndAnimation()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
