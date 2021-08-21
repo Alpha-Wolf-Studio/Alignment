b36859e921d3fa7745c6eb536d29aa61c7d7274b
Adrian <Owinmowe@gmail.com>
2021-08-21 13:32:22 -0300

    Added Melee Attack control tested with Robo Raptor animation.

---
+++ Assets/Scripts/Attack/MeleeAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : AttackComponent
+{
+
+    [SerializeField] List<MeleeAttackCollider> meleeColliders = new List<MeleeAttackCollider>();
+    Collider col;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public override void Attack(Vector3 dir)
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    void StartCollidersDamage() 
+    {
+        foreach (var collider in meleeColliders)
+        {
+            collider.StartCollider();
+        }
+    }
+
+    void StopCollidersDamage() 
+    {
+        foreach (var collider in meleeColliders)
+        {
+            collider.StopCollider();
+        }
+    }
+}
---
+++ Assets/Scripts/Attack/MeleeAttackCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackCollider : MonoBehaviour
+{
+    float damage = 0;
+    LayerMask damageMask = default;
+    Collider col;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    public void StartCollider() 
+    {
+        col.enabled = true;
+    }
+    public void StopCollider() 
+    {
+        col.enabled = false;
+    }
+    public void SetColliders(float damage, LayerMask mask) 
+    {
+        this.damage = damage;
+        damageMask = mask;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable damageComponent = other.GetComponent<IDamageable>();
+        if (damageComponent != null)
+        {
+            damageComponent.TakeDamage(damage);
+        }
+    }
+}
