96b221ffe648d63514c7015e8cf896ed294923fd
Federico Olive <FedericoOlive@gmail.com>
2021-08-22 21:10:58 -0300

    Add icons UI. Add GameManager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,18 +5,57 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
-    enum GameStatus { Inization, Game, EndWin, EndLose}
+    public enum GameStatus { Inization, Game, EndWin, EndLose }
+    public GameStatus gameStatus = GameStatus.Inization;
     public Character player;
     public List<ReparableObject> toRepair = new List<ReparableObject>();
+    private int objectsRemaining;
+
     void Start()
     {
-        player.OnDeath += GameOver;
+        LoadGameManager();  // Sacar esta linea cuando se instancie en el menu
+    }
+    void LoadGameManager()
+    {
+        if (!player) player = FindObjectOfType<PlayerController>().GetComponent<Character>();
+        player.OnDeath += PlayerDeath;
+        foreach (ReparableObject obj in toRepair)
+        {
+            obj.OnRepair += RepairShip;
+            objectsRemaining++;
+        }
+
+        ReparableObject[] raparableObjects = FindObjectsOfType<ReparableObject>();
+        toRepair.Clear();
+        for (var i = 0; i < raparableObjects.Length; i++)
+        {
+            toRepair.Add(raparableObjects[i]);
+        }
+    }
+    void UnloadGameManager()
+    {
+        if (player) player.OnDeath -= PlayerDeath;
+    }
+    void PlayerDeath()
+    {
+        gameStatus = GameStatus.EndLose;
+        GameOver();
+    }
+    void RepairShip()
+    {
+        objectsRemaining--;
+        if (objectsRemaining == 0)
+        {
+            gameStatus = GameStatus.EndWin;
+            GameOver();
+        }
     }
     void GameOver()
     {
         player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
     }
 }
+
 public class Global
 {    
     public static bool LayerEquals(LayerMask mask, int layer)
---
+++ Assets/Scripts/Ui/UiRadar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiRadar : MonoBehaviour
+{
+    public List<Transform> enemy = new List<Transform>();
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
