ac969f564be83e39f32f143d1481013f34e8dab6
Federico Olive <FedericoOlive@gmail.com>
2021-09-11 04:36:31 -0300

    Add Art and swap of panel Options.

---
+++ Assets/Scripts/Ui/UiOptions.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiOptions : MonoBehaviour
+{
+    [SerializeField] private Image[] buttons;
+    [SerializeField] private Image[] panels;
+    [Header("Sprites")]
+    [SerializeField] private Sprite[] spritesControls;
+    [SerializeField] private Sprite[] spritesSettings;
+    [SerializeField] private Sprite[] spritesSounds;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void ChangePanelToControls()
+    {
+        OffAllSprites();
+        buttons[0].sprite = spritesControls[0];
+        panels[0].gameObject.SetActive(true);
+    }
+    public void ChangePanelToSettings()
+    {
+        OffAllSprites();
+        buttons[1].sprite = spritesSettings[0];
+        panels[1].gameObject.SetActive(true);
+    }
+    public void ChangePanelToSounds()
+    {
+        OffAllSprites();
+        buttons[2].sprite = spritesSounds[0];
+        panels[2].gameObject.SetActive(true);
+    }
+    void OffAllSprites()
+    {
+        buttons[0].sprite = spritesControls[1];
+        buttons[1].sprite = spritesSettings[1];
+        buttons[2].sprite = spritesSounds[1];
+
+        for (int i = 0; i < panels.Length; i++)
+            panels[i].gameObject.SetActive(false);
+    }
+}
