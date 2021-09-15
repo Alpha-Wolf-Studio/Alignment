da38f340ee04afc56ac1307b8d5d8590146bf9bd
Adrian <Owinmowe@gmail.com>
2021-09-14 23:10:50 -0300

    Hotfix property drawer using string type to nameof.

--- Assets/Editor/TaskDrawer.cs
+++ Assets/Editor/TaskDrawer.cs
@@ -11,15 +11,15 @@ public class TaskDrawer : PropertyDrawer
 
         EditorGUI.BeginProperty(position, label, property);
 
-        Rect labelRec = new Rect(position.x, position.y, position.width, position.height);
+        //Rect labelRec = new Rect(position.x, position.y, position.width, position.height);
         //position = EditorGUI.PrefixLabel(labelRec, GUIUtility.GetControlID(FocusType.Passive), label);
 
         int indent = EditorGUI.indentLevel;
         EditorGUI.indentLevel = 1;
 
-        Task.TaskType taskTypeEnum = (Task.TaskType)property.FindPropertyRelative("type").enumValueIndex;
+        Task.TaskType taskTypeEnum = (Task.TaskType)property.FindPropertyRelative(nameof(Task.type)).enumValueIndex;
         Rect typeRec = new Rect(position.x, position.y + 10, position.width, position.height * .25f);
-        EditorGUI.PropertyField(typeRec, property.FindPropertyRelative("type"));
+        EditorGUI.PropertyField(typeRec, property.FindPropertyRelative(nameof(Task.type)));
 
         Rect property1Rec = new Rect(position.x, position.y + 35, position.width, position.height * .25f);
         Rect property2Rec = new Rect(position.x, position.y + 60, position.width, position.height * .2f);
@@ -29,22 +29,22 @@ public class TaskDrawer : PropertyDrawer
         {
             case Task.TaskType.KILL:
                 GUI.backgroundColor = Color.red;
-                EditorGUI.PropertyField(property1Rec, property.FindPropertyRelative("dinosaursToKill"));
-                EditorGUI.PropertyField(property2Rec, property.FindPropertyRelative("killAmount"));
+                EditorGUI.PropertyField(property1Rec, property.FindPropertyRelative(nameof(Task.dinosaursToKill)));
+                EditorGUI.PropertyField(property2Rec, property.FindPropertyRelative(nameof(Task.killAmount)));
                 break;
             case Task.TaskType.PICKUP:
                 GUI.backgroundColor = Color.yellow;
-                EditorGUI.PropertyField(propertyItemRec, property.FindPropertyRelative("itemToPickUp"));
-                EditorGUI.PropertyField(property2Rec, property.FindPropertyRelative("pickUpAmount"));
+                EditorGUI.PropertyField(propertyItemRec, property.FindPropertyRelative(nameof(Task.itemToPickUp)));
+                EditorGUI.PropertyField(property2Rec, property.FindPropertyRelative(nameof(Task.pickUpAmount)));
                 break;
             case Task.TaskType.CRAFT:
                 GUI.backgroundColor = Color.green;
-                EditorGUI.PropertyField(propertyItemRec, property.FindPropertyRelative("itemToCraft"));
-                EditorGUI.PropertyField(property2Rec, property.FindPropertyRelative("craftAmount"));
+                EditorGUI.PropertyField(propertyItemRec, property.FindPropertyRelative(nameof(Task.itemToCraft)));
+                EditorGUI.PropertyField(property2Rec, property.FindPropertyRelative(nameof(Task.craftAmount)));
                 break;
             case Task.TaskType.REPAIR:
                 GUI.backgroundColor = Color.blue;
-                EditorGUI.PropertyField(property1Rec, property.FindPropertyRelative("locationToRepair"));
+                EditorGUI.PropertyField(property1Rec, property.FindPropertyRelative(nameof(Task.locationToRepair)));
                 break;
             default:
                 break;
@@ -55,7 +55,14 @@ public class TaskDrawer : PropertyDrawer
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        return 100f;
+        Task.TaskType taskTypeEnum = (Task.TaskType)property.FindPropertyRelative(nameof(Task.type)).enumValueIndex;
+        if(taskTypeEnum == Task.TaskType.REPAIR) 
+        {
+            return 70f;
+        }
+        else 
+        {
+            return 100f;
+        }
     }
-
 }
