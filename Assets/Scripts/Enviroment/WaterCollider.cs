5d05092b7806c545829adb7e3f5d57635dd1ab7e
Adrian <Owinmowe@gmail.com>
2021-09-27 09:54:47 -0300

    Added kill collider to the water.

---
+++ Assets/Scripts/Enviroment/WaterCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollider : MonoBehaviour
+{
+    [SerializeField] float killDamage = 1000f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        IDamageable damageable = collision.collider.GetComponent<IDamageable>(); 
+        if(damageable != null) 
+        {
+            damageable.TakeEnergyDamage(killDamage);
+        }
+    }
+}
