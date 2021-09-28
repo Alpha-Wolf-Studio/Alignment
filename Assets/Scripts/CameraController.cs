0ace93a0fba579769756b7cf27198920a3709693
Adrian <Owinmowe@gmail.com>
2021-09-28 15:50:28 -0300

    Added camera shake on damage recieved.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] Character playerCharacter;
+
+    [Header("Camera Shake")]
+    [SerializeField] float shakeTime = .25f;
+    [SerializeField] float shakeStrenght = .5f;
+    bool shaking = false;
+    private void Awake()
+    {
+        playerCharacter.OnCharacterTakeArmorDamage += CameraShake;
+        playerCharacter.OnCharacterTakeEnergyDamage += CameraShake;
+    }
+
+    void CameraShake() 
+    {
+        if(!shaking) 
+        {
+            StartCoroutine(CameraShakeCoroutine());
+        }
+    }
+
+    IEnumerator CameraShakeCoroutine() 
+    {
+        float t = 0;
+        Vector3 startingPos = transform.position;
+        shaking = true;
+        do
+        {
+            t += Time.deltaTime;
+            Vector3 rand = UnityEngine.Random.insideUnitSphere * shakeStrenght;
+            transform.position = new Vector3(startingPos.x + rand.x, startingPos.y + rand.y, startingPos.z);
+            yield return null;
+        } while (t < shakeTime);
+        transform.position = startingPos;
+        shaking = false;
+    }
+}
