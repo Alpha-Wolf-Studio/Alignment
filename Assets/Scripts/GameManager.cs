d534f6102aee23dac238848b3a49bd0fb393eb51
Adrian <Owinmowe@gmail.com>
2021-09-29 12:27:49 -0300

    Moved death string logic into own class.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,30 +42,7 @@ public class GameManager : MonoBehaviour
     void PlayerDeath(DamageOrigin origin)
     {
         player.playerStatus = PlayerController.PlayerStatus.EndLose;
-        var gameOverText = "";
-        switch (origin)
-        {
-            case DamageOrigin.PLAYER:
-                gameOverText = "Spamming shoot kills you.\nPay attention to your overload meter to the side\nof your aim marker.";
-                break;
-            case DamageOrigin.RAPTOR:
-                gameOverText = "Raptor are balanced dinos.\nKeep your distance and kill them one by one.";
-                break;
-            case DamageOrigin.TRICERATOPS:
-                gameOverText = "Triceratops fling you into the air after each hit.\nTry to evade after they start charging.";
-                break;
-            case DamageOrigin.DILOPHOSAURUS:
-                gameOverText = "Dilophosaurus shoot energy bolts.\nAttack in between their range attacks.";
-                break;
-            case DamageOrigin.COMPSOGNATHUS:
-                gameOverText = "Compsognathus are weak by they move in groups.\nMove constantly while attacking.";
-                break;
-            case DamageOrigin.WATER:
-                gameOverText = "Water kills robots immediately.\n Stay away from water.";
-                break;
-            default:
-                break;
-        }
+        var gameOverText = UIGameOverScreen.GetGameOverText(origin);
         GameOver(gameOverText);
     }
     void RepairShip(RepairLocations location)
---
+++ Assets/Scripts/Ui/UIGameOverScreen.cs
@@ -0,0 +1,33 @@
+﻿public class UIGameOverScreen
+{
+    private UIGameOverScreen() { } //Previene instanciar esta clase
+    public static string GetGameOverText(DamageOrigin origin) 
+    {
+        var gameOverText = "";
+        switch (origin)
+        {
+            case DamageOrigin.PLAYER:
+                gameOverText = "Spamming shoot kills you.\nPay attention to your overload meter to the side\nof your aim marker.";
+                break;
+            case DamageOrigin.RAPTOR:
+                gameOverText = "Raptor are balanced dinos.\nKeep your distance and kill them one by one.";
+                break;
+            case DamageOrigin.TRICERATOPS:
+                gameOverText = "Triceratops fling you into the air after each hit.\nTry to evade after they start charging.";
+                break;
+            case DamageOrigin.DILOPHOSAURUS:
+                gameOverText = "Dilophosaurus shoot energy bolts.\nAttack in between their range attacks.";
+                break;
+            case DamageOrigin.COMPSOGNATHUS:
+                gameOverText = "Compsognathus are weak by they move in groups.\nMove constantly while attacking.";
+                break;
+            case DamageOrigin.WATER:
+                gameOverText = "Robots don't like water.\n Stay away from water.";
+                break;
+            default:
+                break;
+        }
+        return gameOverText;
+    }
+
+}
