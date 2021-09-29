8218c6c44aa5a1ec4dd36ee257fda6e79355b2d5
Adrian <Owinmowe@gmail.com>
2021-09-29 15:24:24 -0300

    Added post processing effect to the camera and shake effect.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,18 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class CameraController : MonoBehaviour
 {
 
     [SerializeField] Character playerCharacter;
 
-    [Header("Camera Shake")]
+    [Header("Shake")]
     [SerializeField] float shakeTime = .25f;
     [SerializeField] float shakeStrenght = .5f;
+
+    [Header("Post Processing")]
+    [SerializeField] PostProcessVolume postProcessVolume;
+    DepthOfField depthOfField;
+    ChromaticAberration chromaticAberration;
+    float startingChromaticValue;
+
     bool shaking = false;
     private void Awake()
     {
+
+        AddPostProccesingReferences();
         playerCharacter.OnCharacterTakeArmorDamage += CameraShake;
         playerCharacter.OnCharacterTakeEnergyDamage += CameraShake;
     }
@@ -31,6 +41,7 @@ public class CameraController : MonoBehaviour
         float t = 0;
         Vector3 startingPos = transform.localPosition;
         shaking = true;
+        ApplyShakePostProccesing();
         do
         {
             t += Time.deltaTime;
@@ -40,5 +51,27 @@ public class CameraController : MonoBehaviour
         } while (t < shakeTime);
         transform.localPosition = startingPos;
         shaking = false;
+        UnapplyShakePostProccesing();
     }
+
+    void AddPostProccesingReferences() 
+    {
+        postProcessVolume.profile.TryGetSettings(out depthOfField);
+        postProcessVolume.profile.TryGetSettings(out chromaticAberration);
+        startingChromaticValue = chromaticAberration.intensity;
+        depthOfField.enabled.value = false;
+    }
+
+    void ApplyShakePostProccesing() 
+    {
+        depthOfField.enabled.value = true;
+        chromaticAberration.intensity.value = 1;
+    }
+
+    void UnapplyShakePostProccesing()
+    {
+        depthOfField.enabled.value = false;
+        chromaticAberration.intensity.value = startingChromaticValue;
+    }
+
 }
