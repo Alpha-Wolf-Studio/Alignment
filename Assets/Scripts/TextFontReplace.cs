065eb6a8faacc0855d28bb3bff831433c3ebf56f
Adrian <Owinmowe@gmail.com>
2021-09-29 13:55:30 -0300

    Changed placeholder font for new one.

---
+++ Assets/Scripts/TextFontReplace.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class TextFontReplace : MonoBehaviour
+{ 
+    [SerializeField] TMP_FontAsset newFontAsset = null;
+    [SerializeField] bool start = false; 
+
+    void Update()
+    {
+        if (start && newFontAsset != null) 
+        {
+            TextMeshProUGUI[] allObjects = GetComponentsInChildren<TextMeshProUGUI>(true);
+            foreach (TextMeshProUGUI text in allObjects) 
+            {
+                text.font = newFontAsset;
+            }                    
+        }
+        start = false;
+    }
+}
