181dc2743d03b25ee30514a11b16b71aa8646650
Federico Olive <FedericoOlive@gmail.com>
2021-10-01 01:35:24 -0300

    Add PopUp de Items.

---
+++ Assets/Scripts/Ui/UiPopUpPickItem.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class UiPopUpPickItem : MonoBehaviour
+{
+    private Image myImage;
+    public Image imageItem;
+    public TextMeshProUGUI textName;
+    public TextMeshProUGUI textAmount;
+    private float ontime;
+    public float maxTime;
+    private UiPopUps uiPopUp;
+
+    private void Awake()
+    {
+        myImage = GetComponent<Image>();
+    }
+    public void SetValues(Sprite image, string nameItem, string amountItem, UiPopUps uiPopUps)
+    {
+        uiPopUp = uiPopUps;
+        imageItem.sprite = image;
+        textName.text = nameItem;
+        textAmount.text = amountItem;
+    }
+    private void Update()
+    {
+        ontime += Time.deltaTime;
+        myImage.color = Color.Lerp(Color.white, Color.clear, ontime / maxTime);
+        if (ontime > maxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        uiPopUp.uiPopList.Remove(this);
+    }
+}
---
+++ Assets/Scripts/Ui/UiPopUps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class UiPopUps : MonoBehaviour
+{
+    private Inventory inventory;
+    public UiPopUpPickItem itemPopUp;
+    [SerializeField] private float timeShow = 1.5f;
+    [HideInInspector] public List<UiPopUpPickItem> uiPopList = new List<UiPopUpPickItem>();
+    public int maxSizeList = 4;
+
+    private void Awake()
+    {
+        inventory = GameManager.Get().player.GetComponent<Inventory>();
+    }
+    private void Start()
+    {
+        inventory.OnPickUp += PickItem;
+    }
+    private void PickItem(Item item, int amount)
+    {
+        UiPopUpPickItem itempick = Instantiate(itemPopUp, transform);
+        itempick.SetValues(item.icon, item.itemName, amount.ToString(), this);
+        itempick.maxTime = timeShow;
+        uiPopList.Add(itempick);
+
+        CheckMaxList();
+    }
+    void CheckMaxList()
+    {
+        if (maxSizeList > uiPopList.Count)
+            uiPopList.RemoveAt(0);
+    }
+}
