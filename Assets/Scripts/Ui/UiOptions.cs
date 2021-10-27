1a4856e710faad05f046a693362aee404648ecbd
Federico Olive <FedericoOlive@gmail.com>
2021-10-27 10:26:30 -0300

    Add PanelOptions functionalities.

--- Assets/Scripts/Ui/UiOptions.cs
+++ Assets/Scripts/Ui/UiOptions.cs
@@ -1,41 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UiOptions : MonoBehaviour
 {
+    [Header("Components")] 
     [SerializeField] private Image[] buttons;
     [SerializeField] private Image[] panels;
-    [Header("Sprites")]
+
+    [Header("Sprites")] 
     [SerializeField] private Sprite[] spritesControls;
     [SerializeField] private Sprite[] spritesSettings;
     [SerializeField] private Sprite[] spritesSounds;
 
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
+    [Header("Other")]
+    private int qualityLevel;
+    private bool fullScreen = true;
+    [SerializeField] private Vector2Int[] resolutions;
+    private int currentResolution;
+
+    //---------- LeftPanels ----------
     public void ChangePanelToControls()
     {
         OffAllSprites();
         buttons[0].sprite = spritesControls[0];
         panels[0].gameObject.SetActive(true);
     }
+
     public void ChangePanelToSettings()
     {
         OffAllSprites();
         buttons[1].sprite = spritesSettings[0];
         panels[1].gameObject.SetActive(true);
     }
+
     public void ChangePanelToSounds()
     {
         OffAllSprites();
         buttons[2].sprite = spritesSounds[0];
         panels[2].gameObject.SetActive(true);
     }
+
     void OffAllSprites()
     {
         buttons[0].sprite = spritesControls[1];
@@ -44,5 +50,65 @@ public class UiOptions : MonoBehaviour
 
         for (int i = 0; i < panels.Length; i++)
             panels[i].gameObject.SetActive(false);
+    }
+
+    //---------- Controls ----------
+    public void ControlChangeSensitiveHor()
+    {
+
+    }
+    public void ControlChangeSensitiveVer()
+    {
+
+    }
+
+    //---------- General -----------
+    public void GeneralChangeQualityGame()
+    {
+        QualitySettings.SetQualityLevel(qualityLevel);
+    }
+    public void GeneralChangeWindowedMode()
+    {
+        fullScreen = !fullScreen;
+        Screen.fullScreenMode = fullScreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+        Debug.Log("fullScreenMode en: " + Screen.fullScreenMode);
+    }
+    public void GeneralChangeResolution()
+    {
+        int width = resolutions[currentResolution].x;
+        int height = resolutions[currentResolution].y;
+
+        Screen.SetResolution(width, height, fullScreen);
+    }
+    public void GeneralChangeVsync()
+    {
+        QualitySettings.vSyncCount = QualitySettings.vSyncCount == 1 ? 0 : 1;
+        Debug.Log("vSyncCount en: " + QualitySettings.vSyncCount);
+    }
+
+    //---------- Sounds ------------
+    public void SoundToggleGeneral()
+    {
+
+    }
+    public void SoundToggleMusic()
+    {
+
+    }
+    public void SoundToggleEffect()
+    {
+
+    }
+    public void SoundChangeGeneral()
+    {
+
+    }
+    public void SoundChangeMusic()
+    {
+
+    }
+    public void SoundChangeEffect()
+    {
+
     }
 }
