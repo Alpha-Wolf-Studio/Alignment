de7f8d3137562ec207b28520db2523de561932a8
Adrian <Owinmowe@gmail.com>
2021-10-29 00:55:01 -0300

    Added trike start animation and modified AI to include it.

--- Assets/Scripts/Dinos/TriAttackAI.cs
+++ Assets/Scripts/Dinos/TriAttackAI.cs
@@ -8,17 +8,20 @@ public class TriAttackAI : AIAttackModule
     [SerializeField] float chargePushStrenght = 600f;
     [SerializeField] float chargeSpeedMultiplier = 5f;
     [SerializeField] float ChargeStoppingDistance = 2.0f;
-    [SerializeField] float chargeForwardOffset = 10.0f;
+    [SerializeField] float chargeMinStart = 10.0f;
+    [SerializeField] float chargeMinResolveTime = 2.0f;
+    [SerializeField] float chargeForwardOffset = 5.0f;
     float multipliedSpeed = 0;
     float multipliedRotationSpeed = 0;
     IEnumerator ChargeCoroutine = null;
     bool canCharge = true;
+    bool attackStarted = false;
     private void Start()
     {
         multipliedSpeed = agent.Speed * chargeSpeedMultiplier;
         multipliedRotationSpeed = agent.AngularSpeed;
     }
-    public override void Attack(Vector3 dir, DamageOrigin origin)
+    public override void Attack(Vector3 dir, DamageInfo info)
     {
         if (canCharge)
         {
@@ -26,14 +29,13 @@ public class TriAttackAI : AIAttackModule
             {
                 if (collider.GetType() == typeof(ChargeAttackCollider))
                 {
-                    ((ChargeAttackCollider)collider).SetColliders(attackStrenght, chargePushStrenght, origin);
+                    ((ChargeAttackCollider)collider).SetColliders(attackStrenght, chargePushStrenght, info.origin);
                 }
             }
             if (ChargeCoroutine != null)
             {
                 StopCoroutine(ChargeCoroutine);
             }
-            agent.basicNavAgent.isStopped = false;
             ChargeCoroutine = Charge(dir);
             StartCoroutine(ChargeCoroutine);
             AIAttacked();
@@ -44,10 +46,16 @@ public class TriAttackAI : AIAttackModule
         canCharge = false;
         anim.SetBool("Walking", false);
         anim.SetBool("Attacking", true);
-        dir.y = transform.position.y;
+        attackStarted = false;
+        agent.SetDestination(transform.position);
+        while (!attackStarted) 
+        {
+            yield return null;
+        }
+        agent.basicNavAgent.isStopped = false;
         agent.Speed = multipliedSpeed;
         agent.AngularSpeed = multipliedRotationSpeed;
-        if (Vector3.Distance(transform.position, dir) < ChargeStoppingDistance + chargeForwardOffset)
+        if (Vector3.Distance(transform.position, dir) < ChargeStoppingDistance + chargeMinStart)
         {   //En caso de tener al player demasiado cerca al empezar la carga
             float t = 0;
             do
@@ -55,10 +63,11 @@ public class TriAttackAI : AIAttackModule
                 agent.Move(transform.forward * Time.deltaTime * chargeForwardOffset);
                 t += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
-            } while (t < 1);
+            } while (t < chargeMinResolveTime);
         }
         do
         {
+            dir.y = transform.position.y;
             agent.SetDestination(dir);
             agent.Move(transform.forward * Time.deltaTime * chargeForwardOffset); //Tengo un offset para naturalizar la direccion del enemigo
             yield return new WaitForEndOfFrame();
@@ -67,8 +76,10 @@ public class TriAttackAI : AIAttackModule
         agent.basicNavAgent.isStopped = true;
         anim.SetBool("Melee Attack", true);
     }
+
     public override void StartAttackEvent()
     {
+        attackStarted = true;
         AIAttacked();
         foreach (var collider in meleeColliders)
         {
