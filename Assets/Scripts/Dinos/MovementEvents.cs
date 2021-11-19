0293b42228fff27429eaf35e45565370545a391c
Adrian <Owinmowe@gmail.com>
2021-11-19 18:18:33 -0300

    Added step events for dinosaurs (Sounds).

---
+++ Assets/Scripts/Dinos/MovementEvents.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementEvents : MonoBehaviour
+{
+    [SerializeField] EnemyAI enemyAI;
+    DinoType dinoType;
+    uint stepAudioID = 0;
+
+    private void Awake()
+    {
+        dinoType = enemyAI.dinoType;
+        switch (dinoType)
+        {
+            case DinoType.Raptor:
+                stepAudioID = AK.EVENTS.RAPTORSTEP;
+                break;
+            case DinoType.Triceratops:
+                stepAudioID = AK.EVENTS.TRIKESTEP;
+                break;
+            case DinoType.Dilophosaurus:
+                stepAudioID = AK.EVENTS.DILOSTEP;
+                break;
+            case DinoType.Compsognathus:
+                stepAudioID = AK.EVENTS.COMPYSTEP;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnStepEvent() 
+    {
+        AkSoundEngine.PostEvent(stepAudioID, gameObject);
+    }
+}
