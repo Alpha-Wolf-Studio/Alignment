e0b4aa2c1cb9c6dbd4baa84715009635ab95d69d
Federico Olive <FedericoOlive@gmail.com>
2021-11-29 14:35:29 -0300

    Implement Stats on UI.

---
+++ Assets/Scripts/Ui/UiStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+public class UiStats : MonoBehaviour
+{
+    public PlayerController playerController;
+    private CharacterStats playerStats;
+    public TextMeshProUGUI textArmor;
+    public TextMeshProUGUI textDamage;
+    public TextMeshProUGUI textEnergy;
+    public TextMeshProUGUI textDefense;
+    public TextMeshProUGUI textSpeed;
+
+    private void Start()
+    {
+        playerStats = playerController.GetComponent<CharacterStats>();
+        playerController.onInventory += OnInventory;
+    }
+    public void OnInventory()
+    {
+        textArmor.text = playerStats.GetStat(StatType.Armor).GetCurrent().ToString("F0");
+        textDamage.text = playerStats.GetStat(StatType.Damage).GetCurrent().ToString("F0");
+        textEnergy.text = playerStats.GetStat(StatType.Energy).GetCurrent().ToString("F0");
+        textDefense.text = playerStats.GetStat(StatType.Defense).GetCurrent().ToString("F0");
+        textSpeed.text = (playerStats.GetStat(StatType.Walk).GetCurrent() * 100).ToString("F0");
+    }
+}
