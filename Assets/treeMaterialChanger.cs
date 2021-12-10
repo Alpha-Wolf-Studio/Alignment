1b0ef220b4b4d184924b4f482c26b457a420c92d
Adrian <Owinmowe@gmail.com>
2021-12-10 00:36:41 -0300

    Added trees variation (with script for new variations).

---
+++ Assets/treeMaterialChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class treeMaterialChanger : MonoBehaviour
+{
+
+    [SerializeField] List<Material> possibleMaterials = null;
+    [SerializeField] bool reroll = false;
+
+    void Update()
+    {
+        if (reroll) 
+        {
+            var renderers = GetComponentsInChildren<MeshRenderer>();
+            foreach (var rend in renderers)
+            {
+                rend.material = possibleMaterials[Random.Range(0, possibleMaterials.Count)];
+            }
+            reroll = false;
+        }
+    }
+}
